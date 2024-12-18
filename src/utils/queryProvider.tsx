0802fb98c656ee0328250bf77a01a7c8af496a77
GustavoGPL <gustavoguillan23@gmail.com>
2024-12-18 02:59:12 -0300

    .

--- src/utils/queryProvider.tsx
+++ src/utils/queryProvider.tsx
@@ -2,22 +2,17 @@
 
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { redirect, usePathname } from 'next/navigation';
-import Header from '@/components/header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { queryClient } from './react-query';
-import { useSession } from 'next-auth/react';
 
 export default function QueryProvider({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const { data: session } = useSession();
 	return (
 		<QueryClientProvider client={queryClient}>
-			{session?.user && <Header />}
 			{children}
 			<ReactQueryDevtools initialIsOpen={false} />
 			<ToastContainer />
